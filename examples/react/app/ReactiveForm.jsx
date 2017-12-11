832c147758e63815f9a54bc21ef12c4997015770
Karim <kinottohw@gmail.com>
2017-12-11 18:46:47 +0100

    style examples

--- examples/react/app/ReactiveForm.jsx
+++ examples/react/app/ReactiveForm.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ReactiveFormValidator from 'reactive-form-validator';
+import $ from 'jquery';
 
 export default class ReactiveForm extends Component {
   constructor() {
@@ -18,16 +19,23 @@ export default class ReactiveForm extends Component {
         input.dom, // dom element
         input.validator, // validation
         'blur',
-        [], // error classes
+        ['input_err'], // error classes
         100 // new debounce
       );
+
+      let error = $(`
+        <div class="input_err_text">
+          ${input.validator.msg}
+        </div>`);
 
       validator$
         .subscribe(el => {
           if (el.isValid) {
             console.log('is valid');
+            error.remove();
           } else {
-            console.log('is NOT valid');
+            console.log('is NOT valid, change dom accordingly');
+            $(input.dom).parent().append(error);
           }
         });
     });
@@ -37,29 +45,33 @@ export default class ReactiveForm extends Component {
       <div>
         <h2>example form react + reactive form validation</h2>
         <form action="">
-            Name
-          <input
-            type="text"
-            ref={el => this.inputs.push({
-              'dom': el,
-              'validator': {
-                'fn': evt => evt.target.value,
-                'msg': 'name cannot be empty'
-              }
-            })}
-          /><br/>
-            Surname
-          <input
-            type="text"
-            ref={el => this.inputs.push({
-              'dom': el,
-              'validator': {
-                'fn': evt => evt.target.value,
-                'msg': 'surname cannot be empty'
-              }
-            })}
-          />
-          <br/>
+          <div className="input_cont">
+            <input
+              type="text"
+              ref={el => this.inputs.push({
+                'dom': el,
+                'validator': {
+                  'fn': evt => evt.target.value,
+                  'msg': 'name cannot be empty'
+                }
+              })}
+              placeholder="Name"
+            />
+          </div>
+
+          <div className="input_cont">
+            <input
+              type="text"
+              placeholder="Surname"
+              ref={el => this.inputs.push({
+                'dom': el,
+                'validator': {
+                  'fn': evt => evt.target.value,
+                  'msg': 'surname cannot be empty'
+                }
+              })}
+            />
+          </div>
         </form>
       </div>
     );
